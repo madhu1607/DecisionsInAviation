f0ef34964604427ed5ce229700af8f069713d5a9
Madhuri Rudrabhatla <madhu1607@gmail.com>
2021-04-18 02:24:32 +0530

    Starter files and Initial commits

---
+++ CloudCapstone/src/main/java/Task1/XYonTimeArrPerf.java
@@ -0,0 +1,106 @@
+package Task1;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+import Task1.AirlineOntimeMetadata;
+
+public class XYonTimeArrPerf {
+	public static class XYonTimeMapper extends Mapper<LongWritable,Text,Text,DoubleWritable>{
+		
+		private Text keyData = new Text();
+		private DoubleWritable arrDelay = new DoubleWritable();
+		
+		public void map(LongWritable key, Text lineText,Context context) {
+			
+			try {
+				
+			Stream.of(lineText.toString())
+			.map(line -> line.split(","))
+			.forEach(tokens->
+			{
+				//String org = tokens[AirlineOntimeMetadata.ORIGIN_AIRPORT].replaceAll("\"", "");
+				String arrival = tokens[AirlineOntimeMetadata.ARRIVAL_DELAY].replaceAll("\"", "");
+				//String dest = tokens[AirlineOntimeMetadata.DESTINATION_AIRPORT].replaceAll("\"", "");
+				//String carrier = tokens[AirlineOntimeMetadata.UNIQUE_CARRIER_ID].replaceAll("\"", "");
+				
+				
+				//data[0]=org ; data[1] = delay ; data[2]=dest ; data[3]=carrier 
+				String data = tokens[AirlineOntimeMetadata.ORIGIN_AIRPORT].replaceAll("\"", "")+
+						" " + tokens[AirlineOntimeMetadata.DESTINATION_AIRPORT].replaceAll("\"", "")+
+						" " + tokens[AirlineOntimeMetadata.UNIQUE_CARRIER_ID].replaceAll("\"", "");
+				
+				keyData.set(data);
+				arrDelay.set(Double.parseDouble(arrival));
+				
+				try {
+					context.write(keyData, arrDelay);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+			
+			}catch(Exception e) {
+				System.err.println(e);
+			}
+		}
+		
+	}
+	
+	
+	public static class XYonTimeReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable>{
+		
+		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+			
+			int count=0;double sum = 0;
+			for(DoubleWritable val: values) {
+				sum = sum + val.get();
+				count = count + 1;				
+			}
+			
+			Double avg = sum/count;
+			context.write(key,new DoubleWritable(avg));
+		}
+	}
+
+	public static void main(String args[])throws Exception {
+		Job job = Job.getInstance();
+	    job.setJarByClass(XYonTimeArrPerf.class);
+	    job.setJobName("Frequency");
+
+	    // set the input and output path
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+	    // set the Mapper and Reducer class
+	    job.setMapperClass(XYonTimeMapper.class);
+	    job.setCombinerClass(XYonTimeReducer.class);
+	    job.setReducerClass(XYonTimeReducer.class);
+
+	    // specify the type of the output
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(DoubleWritable.class);
+
+	    // run the job
+	    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	  
+		
+	}
+
+
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Aviation Analysis :airplane:
+In this project I use various big data tools like hadoop, Spark and Cassandra for analysis and storing of data to answer a particular set of day to day questions in Aviation operations such as:
+* the best flight on a given day
+* the most popular airports
+* the most on-time airlines, etc. 
+
+## Technologies and implementation
+1. Data Extraction and Cleaning - Extracting csv files to HDFS
+1. Data Analysis using Hadoop and PySpark
+1. MapReduce codes in java
+1. Process Mapreduce operation using Pyspark
+1. Store mapreduce outputs in a dataframe into a cassandra table to retrieve our required results.
+<br>
+*I ran cassandra locally as the size of my datasets and scope of my project was quite small*
+
